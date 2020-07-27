bb3a11b22985054d301adf3adef0eddef666179a
nprate2 <42984229+nprate2@users.noreply.github.com>
2020-07-27 17:56:03 -0500

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -3,6 +3,6 @@ var svg = d3.select('body').append('svg').attr('width',200).attr('height',200);
 svg.append('g').attr("transform","translate(50,50)").append('circle')
 .attr('cx', 100)
 .attr('cy', 100)
-.attr('r', 10)
+.attr('r', 10);
 
 }
