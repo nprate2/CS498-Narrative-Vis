f444b435b46c026a828852972666b832cb06c787
nprate2 <42984229+nprate2@users.noreply.github.com>
2020-07-27 18:47:18 -0500

    test

--- scripts.js
+++ scripts.js
@@ -1,4 +1,5 @@
 async function initPlot() {
+const data = await d3.csv('https://github.com/nprate2/CS498-Narrative-Vis/blob/master/WHO_Covid19_Cases_Deaths_USA.csv');
 var svg = d3.select('body').append('svg').attr('width',200).attr('height',200);
 svg.append('g').attr("transform","translate(50,50)").append('circle')
 .attr('cx', 100)
