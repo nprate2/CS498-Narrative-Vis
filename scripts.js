99c57b34ce81e58ddd45c8838e1d634224faa0f6
nprate2 <42984229+nprate2@users.noreply.github.com>
2020-07-27 17:54:52 -0500

    test

--- scripts.js
+++ scripts.js
@@ -1,8 +1,8 @@
 async function initPlot() {
-
-d3.select('svg').append('g').attr("transform","translate(50,50)").append('circle')
+var svg = d3.select('body').append('svg').attr('width',200).attr('height',200);
+svg.append('g').attr("transform","translate(50,50)").append('circle')
 .attr('cx', 100)
 .attr('cy', 100)
-.attr('r', 100)
+.attr('r', 10)
 
 }
