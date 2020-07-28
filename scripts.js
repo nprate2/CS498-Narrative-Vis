fe2c331c9a4c1f809733084823f1ce87e3a04e5e
nprate2 <42984229+nprate2@users.noreply.github.com>
2020-07-28 16:26:51 -0500

    d

--- scripts.js
+++ scripts.js
@@ -9,16 +9,6 @@ async function initPlot() {
   // Generates SVG canvas
   d3.select("svg").append("g").attr("transform", "translate(" + margin.left + "," + margin.top + ")")
   .append("rect").attr("x", 100).attr("y", 100).attr("width", 100).attr("height", 100);
+
   
-  const data = await d3.csv('./WHO_Covid19_Cases_Deaths_USA.csv');
-  d3.select('body').selectAll('p').data(data).enter().append('p');
-
-  var xScale = d3.scaleBand().domain(data.NewCase).range([0,width]);
-  var yScale = d3.scaleLinear().domain([0,d3.max(data.NewCase)]).range([height,0]);
-
-  d3.select('svg').append('g').attr("transform","translate(50,50)")
-  .call(d3.axisLeft(yScale).tickFormat(d3.format("~s")));
-
-  d3.select('svg').append('g').attr("transform","translate(50,250)")
-  .call(d3.axisBottom(xScale).tickFormat(d3.format("~s")));
 }
