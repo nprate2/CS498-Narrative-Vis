a91ac25551a37d07c0452e5b0dee5cdb1779a50b
nprate2 <42984229+nprate2@users.noreply.github.com>
2020-07-28 15:01:12 -0500

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -3,20 +3,22 @@ async function initPlot() {
   var margin = {top: 50, right: 50, bottom: 50, left: 50},
     width = 600 - margin.left - margin.right,
     height = 600 - margin.top - margin.bottom;
+
   // Used to parse date when reading data
   //var parseDate = d3.time.format("%d-%b-%y").parse;
   // Generates SVG canvas
   const data = await d3.csv('./WHO_covid19_new_cases.csv');
-  var xScale = d3.scaleBand().domain(data).range([0,width])
-  var yScale = d3.scaleLinear().domain([0,d3.max(data)]).range([height,0])
-  d3.select('body').selectAll('p').data(data).enter().append('p')
+  d3.select('body').selectAll('p').data(data).enter().append('p');
 
-  var svg = d3.select("body").append("svg").attr("width", width + margin.left + margin.right)
+  var xScale = d3.scaleBand().domain(data).range([0,width]);
+  var yScale = d3.scaleLinear().domain([0,d3.max(data)]).range([height,0]);
+
+  d3.select("body").append("svg").attr("width", width + margin.left + margin.right)
   .attr("height", height + margin.top + margin.bottom)
   .append("g").attr("transform", "translate(" + margin.left + "," + margin.top + ")")
   .selectAll("rect").data(data).enter().append("rect")
-  .attr("x", function(d, i) {return x(i)})
-  .attr("y", function(d) {return y(d)})
-  .attr("width", x.bandwidth)
-  .attr("height", function(d) { return 200 - y(d)})
+  .attr("x", function(d, i) {return xScale(i)})
+  .attr("y", function(d) {return yScale(d)})
+  .attr("width", xScale.bandwidth)
+  .attr("height", function(d) { return 200 - yScale(d)});
 }
