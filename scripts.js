a51ea1152b9fbb2dfcb8220d767e441ce4a5723f
nprate2 <42984229+nprate2@users.noreply.github.com>
2020-07-28 00:22:38 -0500

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -1,8 +1,8 @@
 async function initPlot() {
   // Margins and Svg Dimensions
-  var margin = {top: 30, right: 20, bottom: 30, left: 50},
+  var margin = {top: 50, right: 50, bottom: 50, left: 50},
     width = 600 - margin.left - margin.right,
-    height = 270 - margin.top - margin.bottom;
+    height = 600 - margin.top - margin.bottom;
   // Used to parse date when reading data
   var parseDate = d3.time.format("%d-%b-%y").parse;
   // Generates SVG canvas
@@ -10,6 +10,7 @@ async function initPlot() {
     .attr("width", width + margin.left + margin.right)
     .attr("height", height + margin.top + margin.bottom)
     .append("g").attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+  svg.append("circle").attr("cx", 200).attr("cy", 200).attr("cr", 10);
   // Read and use data
   d3.csv('https://github.com/nprate2/CS498-Narrative-Vis/blob/master/WHO_Covid19_Cases_Deaths_USA.csv', function(error, data) {
       data.forEach(function(d) {
